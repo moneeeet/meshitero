ba070e13db86d1f7b55b6a656081184a023230b5
EC2 Default User <ec2-user@ip-172-31-74-48.ec2.internal>
2022-06-16 00:41:47 +0000

    2

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,8 +1,6 @@
-class User < ApplicationRecord
+class View < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :post_images, dependent: :destroy
 end
